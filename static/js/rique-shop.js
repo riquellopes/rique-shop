40370e676483a5ca51d90f44720c67c6bbefa1b1
Henrique Lopes <riquellopes@gmail.com>
2013-09-05 00:06:07 -0300

    Add a lib Mustache a appp

--- static/js/rique-shop.js
+++ static/js/rique-shop.js
@@ -1,16 +1,34 @@
 RiqueShop=function(){
+	var tpl = function(){
+		var tpl = new Array();
+			tpl.push('<ul>');
+				tpl.push('{{#results}}');
+					tpl.push('<li>{{title}}</li>');
+				tpl.push('{{results}}');	
+			tpl.push('</ul>');
+		return tpl.join('').trim();
+	}();
+	
 	this.init=function(){
 		$('form').on('submit', function(){
-			var text = $('input:text', this),
+			var data = {},
+				text = $('input:text', this),
 				url = "/search/".concat(text.val().replace(/ /g, '+'));
 					  text.attr('disabled', 'disabled');
 				
-			$.get(url, function(data){
-				/**
-				 * @todo código da tpl deve ser adicionado aqui!
-				 */
-				text.removeAttr('disabled');
+			$.get(url, function(rdata){
+				$.extend(rdata, data);
 			});
+			
+			$(document).ajaxStop(function(){
+				var render = Mustache.to_html( tpl, data );
+					
+					/**
+					 * @todo código da tpl deve ser adicionado aqui!
+					 */
+					text.removeAttr('disabled');
+					$('#result').html(render);
+			})
 		});
 	}
 	
